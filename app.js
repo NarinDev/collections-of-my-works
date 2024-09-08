f1f12b96405033911461968a45932ce66b341cb8
NarinDev <ninny.dt@gmail.com>
2024-09-08 18:39:42 +0500

    Добавление новых файлов.

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const app = express();
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use((request, respose) => {
-    respose.status(404);
-    console.log(__dirname)
-    //console.log(request)
-    respose.send(`<h1>Error 404</h1>`)
+app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "public", "index.html"))
 })
+// app.use((request, respose) => {
+//     respose.status(404);
+//     console.log(__dirname)
+//     //console.log(request)
+//     respose.send(`<h1>Error 404</h1>`)
+// })
 app.listen(3000, () => {
     console.log('app listening on port 3000')
 })
---
+++ public/JS/mainPage.js
@@ -0,0 +1,9 @@
+console.log("%c++","background:green","HERE")
+
+const btnLogin = document.getElementById("btnLogin");
+console.log("%c++","background:green",btnLogin);
+btnLogin.addEventListener("click", () => {
+    console.log("audio")
+    const audio = new Audio("../Sounds/sound.wav")
+    console.log("%c++","background:red",)
+})
