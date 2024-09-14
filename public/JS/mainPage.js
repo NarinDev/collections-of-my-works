510c71b8f522abacdd9836e8db3a6d6c5f0a1743
NarinDev <ninny.dt@gmail.com>
2024-09-14 15:55:20 +0500

    Добавление гирлянды.

--- public/JS/mainPage.js
+++ public/JS/mainPage.js
@@ -4,6 +4,6 @@ const btnLogin = document.getElementById("btnLogin");
 console.log("%c++","background:green",btnLogin);
 btnLogin.addEventListener("click", () => {
     console.log("audio")
-    const audio = new Audio("../Sounds/sound.wav")
+    const audio = new Audio("../Sounds/sound.wav").play()
     console.log("%c++","background:red",)
 })
